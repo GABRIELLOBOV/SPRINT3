984c0d85159c2eded3aa8efaed50326847006a9e
GABRIELLOBOV <lobovarela@gmail.com>
2024-12-19 11:32:25 -0300

    SuperHero.mjs

--- models/SuperHero.mjs
+++ models/SuperHero.mjs
@@ -1,19 +1,18 @@
 import mongoose from 'mongoose';
 
 const superheroSchema = new mongoose.Schema({
-    nombreSuperHeroe: { type: String, required: true },
-    nombreReal: { type: String, required: true },
-    edad: { type: Number, min: 0 },
-    planetaOrigen: { type: String, default: 'Desconocido' },
-    debilidad: [String],
-    poderes: [String],
-    aliados: [String],
-    enemigos: [String],
-    creador: [String],
+    nombreSuperHeroe: { type: String, required: true }, 
+    nombreReal: { type: String, required: true },       
+    edad: { type: Number, min: 0, required: true },     
+    planetaOrigen: { type: String, default: 'Desconocido' }, 
+    poderes: { type: [String], required: true },        
+    debilidad: { type: [String], required: true },      
+    aliados: { type: [String], default: [] },           
+    enemigos: { type: [String], default: [] },          
+    creador: { type: [String], default: [] },           
 }, {
-    timestamps: true, // Crea campos "createdAt" y "updatedAt"
-    collection: 'Grupo-12'
+    timestamps: true, 
+    collection: 'Grupo-12', 
 });
 
 export default mongoose.model('SuperHero', superheroSchema);
-
